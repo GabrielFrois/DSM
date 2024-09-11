cd4787234defe5fee399cf3b24f7c0a77855982d
Gabriel Frois <68859749+GabrielFrois@users.noreply.github.com>
2024-09-11 19:38:26 -0300

    Create ex1.js

---
+++ Algoritmo-e-Logica-de-Programacao/Aula-5/ex1.js
@@ -0,0 +1,7 @@
+/*Completar o código a seguir para imprimir na tela o
+primeiro elemento do array.
+numeros = ["um","dois","três","quatro"]*/
+
+numeros = ["um","dois","três","quatro"]
+
+console.log(numeros[0]);
