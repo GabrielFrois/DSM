9df7f9f8fc5d74643a5f0c736ef6fd1a1026edd1
Gabriel Frois <68859749+GabrielFrois@users.noreply.github.com>
2024-09-27 20:08:59 -0300

    Create exercicios.sql

---
+++ Banco-de-Dados/Aula-7/exercicios.sql
@@ -0,0 +1,30 @@
+-- Ex 1: Fazer uma consulta para listar a quantidade de cursos por unidade. Apresente o resultado ordenado em ordem alfabética. Renomear as colunas para Fatec e Quantidade.
+-- O resultado terá 75 registros. Dica: use o termo group by e função agrupadora count.
+
+-- Ex 2: Alterar a consulta do Exercício 1 para mostrar o resultado ordenado em ordem decrescente dequantidade.
+-- O resultado terá 75 registros. Dica: ordene pela quantidade.
+
+
+-- Ex 3: Alterar a consulta do Exercício 2 para mostrar apenas o registro que possui a maior quantidade.
+-- O resultado terá 1 registro. Dica: use o termo limit
+
+-- Ex 4: Alterar a consulta do Exercício 3 para mostrar apenas o registro que possui a segunda maior quantidade.
+-- O resultado terá 1 registro. Dica: use o termo offset.
+
+-- Ex 5: Alterar a consulta do Exercício 1 para mostrar apenas as unidades que possuem exatamente três cursos.
+-- O resultado terá 9 registros. Dica: use o termo having.
+
+-- Ex 6: Fazer uma consulta para listar a quantidade de cursos por turno e unidade. Apresente o resultado ordenado em ordem alfabética. Renomear as colunas para Fatec e Quantidade.
+-- O resultado terá 211 registros.
+
+-- Ex 7: Alterar a consulta do Exercício 6 para listar somente as unidades que possuem cinco cursos no mesmo turno.
+-- O resultado terá 7 registros.
+
+-- Ex 8: Alterar a consulta do Exercício 6 para listar somente o resultado de São José dos Campos.
+-- O resultado terá 3 registros
+
+-- Ex 9: Alterar a consulta do Exercício 8 para listar a quantidade total de vagas por turno.
+-- O resultado terá 3 registros. Dica: use a função sum.
+
+-- Ex 10: Fazer uma consulta para listar a quantidade total de vagas por turno considerando todas as unidades e cursos.
+-- O resultado terá 4 registros.
