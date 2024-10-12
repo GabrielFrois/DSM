ef032915b9bee13075c9a528b5676d5fbf211dfc
Gabriel Frois <68859749+GabrielFrois@users.noreply.github.com>
2024-10-11 22:48:27 -0300

    Update exercicios.sql

--- Banco-de-Dados/Aula-8/exercicios.sql
+++ Banco-de-Dados/Aula-8/exercicios.sql
@@ -2,22 +2,43 @@
 -- where da cláusula somente o nome do município. Apresente o resultado ordenado em ordem alfabética.
 -- Renomear a coluna para Curso. Observação: você sabe apenas o nome do município, ou seja, você não sabe que o id da unidade é 33.
 -- O resultado terá 3 registros.
-
+SELECT curso.nome AS "Curso" FROM tbcurso AS curso, tbunidade AS unidade, tbcurso_por_unidade AS cpu
+WHERE unidade.id = cpu.idunidade -- Faz a junção entre tbunidade e tbcurso_por_unidade onde os IDs da unidade correspondem
+AND curso.id = cpu.idcurso -- Faz a junção entre tbcurso e tbcurso_por_unidade onde os IDs dos cursos correspondem
+AND unidade.municipio = 'Jacareí'
+ORDER BY curso.nome;
 
 -- Ex 2: Alterar a consulta do Exercício 1 para mostrar o turno de cada curso.
 -- O resultado terá 3 registros.
+SELECT curso.nome AS "Curso", turno.nome AS "Turno" 
+FROM tbcurso AS curso, tbunidade AS unidade, tbcurso_por_unidade AS cpu, tbturno AS turno
+WHERE unidade.id = cpu.idunidade
+AND curso.id = cpu.idcurso
+AND turno.id = cpu.idturno
+AND unidade.municipio = 'Jacareí'
+ORDER BY curso.nome;
 
 
 -- Ex 3:  Fazer uma consulta para listar as unidades que possuem o curso de Gestão Financeira. Apresente o
 -- resultado ordenado em ordem alfabética e evitar nomes repetidos. Renomear a coluna para Unidade.
 -- Observação: você sabe apenas o nome do curso, ou seja, você não sabe que o id do curso.
 -- O resultado terá 3 registros. Dica: use o termo distinct para evitar repetições
+SELECT DISTINCT unidade AS "Unidade" FROM tbcurso AS curso, tbunidade AS unidade, tbcurso_por_unidade AS cpu
+WHERE unidade.id = cpu.idunidade
+AND curso.id = cpu.idcurso
+AND curso.nome = 'Gestão Financeira'
+ORDER BY unidade;
 
 
 -- Ex 4: Fazer uma consulta para listar os turnos que possuem o curso de Gestão Financeira. Apresente o
 -- resultado ordenado em ordem alfabética e evitar nomes repetidos. Renomear a coluna para Turno.
 -- Observação: você sabe apenas o nome do curso, ou seja, você não sabe que o id do curso.
 -- O resultado terá 2 registros. Dica: use o termo distinct para evitar repetições.
+SELECT DISTINCT turno.nome AS "Turno" FROM tbcurso AS curso, tbturno AS turno, tbcurso_por_unidade AS cpu
+WHERE turno.id = cpu.idturno
+AND curso.id = cpu.idcurso
+AND curso.nome = 'Gestão Financeira'
+ORDER BY turno.nome;
 
 
 -- Ex 5: Fazer uma consulta para listar a quantidade de cursos por turno. Apresente o resultado ordenado em
