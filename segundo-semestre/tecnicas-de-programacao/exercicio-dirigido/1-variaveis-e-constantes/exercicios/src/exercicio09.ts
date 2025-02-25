d6472228e081d7f088e21068d5729cb2ca7c1d2f
Gabriel Frois <68859749+GabrielFrois@users.noreply.github.com>
2025-02-25 18:51:02 -0300

    Delete segundo-semestre/tecnicas-de-programacao/exercicio-dirigido directory

--- segundo-semestre/tecnicas-de-programacao/exercicio-dirigido/1-variaveis-e-constantes/exercicios/src/exercicio09.ts
+++
@@ -1,5 +0,0 @@
-const f: number = 212;
-
-const c: number = ((f - 32) / 9) * 5;
-
-console.log(`${f}°F = ${c.toFixed(1)}°C`);
--- segundo-semestre/tecnicas-de-programacao/exercicio-dirigido/1-variaveis-e-constantes/exercicios/src/index.ts
+++
@@ -1,8 +0,0 @@
-let area: number, base:number, altura:number;
-
-base = 3;
-altura = 4;
-
-area = base * altura / 2;
-
-console.log("A área do triângulo é", area);
