39b91a351e30b72187ae47a7928aa7113571e673
Gabriel Frois <68859749+GabrielFrois@users.noreply.github.com>
2025-02-25 18:52:26 -0300

    Add files via upload

---
+++ segundo-semestre/tecnicas-de-programacao/aula-2-classes-e-objetos/exercicio-dirigido/exercicios/src/exercicio05.ts
@@ -0,0 +1,6 @@
+const base: number = 3;
+const altura: number = 5;
+
+const area: number = base * altura;
+
+console.log(`A área é: ${area}`);
---
+++ segundo-semestre/tecnicas-de-programacao/aula-2-classes-e-objetos/exercicio-dirigido/exercicios/src/exercicio09.ts
@@ -0,0 +1,5 @@
+const f: number = 212;
+
+const c: number = ((f - 32) / 9) * 5;
+
+console.log(`${f}°F = ${c.toFixed(1)}°C`);
